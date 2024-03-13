d9d6edb321058c34732e0552649b8f1e69e4b351
Sylvia van Os <sylvia@hackerchick.me>
2024-03-13 18:39:39 +0100

    Follow MaxMind redirect
    On May 1st, MaxMind will transition to serving the databases from Cloudflare CDN using "R2 Presigned URLs", using forwards from the original download links

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,22 +4,22 @@ DB=/db/GeoLite2-City.mmdb
 DB_TMP=/tmp/GeoLite2-City.mmdb
 
 update_db_if_needed() {
-# $1 = clear cache
-if [ ! -f "$DB" ]; then
-    echo "${DB} not found. Downloading..."
+    # $1 = clear cache
+    if [ ! -f "$DB" ]; then
+        echo "${DB} not found. Downloading..."
 
-    download_db
-elif [ $(( $(date +%s) - $(stat -c '%Y' ${DB}) )) -gt 604800 ]; then
-    echo "${DB} is older than a week. Downloading update..."
+        download_db
+    elif [ $(( $(date +%s) - $(stat -c '%Y' ${DB}) )) -gt 604800 ]; then
+        echo "${DB} is older than a week. Downloading update..."
 
-    download_db
+        download_db
 
-    if [ "$1" -eq 1 ]; then
-        curl http://localhost:8000/clear_cache
+        if [ "$1" -eq 1 ]; then
+            curl http://localhost:8000/clear_cache
+        fi
+    else
+        echo "${DB} is up-to-date"
     fi
-else
-    echo "${DB} is up-to-date"
-fi
 }
 
 download_db() {
@@ -29,7 +29,7 @@ download_db() {
     fi
 
     echo "Downloading GeoLite2-City.mmdb"
-    curl "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=${SIMPLE_GEOIP_FORWARDAUTH_MAXMIND_LICENSE_KEY}&suffix=tar.gz" | tar -zxf - --wildcards '*/GeoLite2-City.mmdb' -O > "${DB_TMP}"
+    curl -L "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=${SIMPLE_GEOIP_FORWARDAUTH_MAXMIND_LICENSE_KEY}&suffix=tar.gz" | tar -zxf - --wildcards '*/GeoLite2-City.mmdb' -O > "${DB_TMP}"
 
     echo "Testing new DB"
     if python3 test_db.py "${DB_TMP}"; then
